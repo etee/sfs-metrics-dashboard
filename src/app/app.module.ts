23f107a0f8240733c79116625065a74d0d180e99
Etee Kushwaha <eteekushwaha@gmail.com>
2025-04-04 20:10:44 +0530

    Initialize Angular project with basic structure and components

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DevicesComponent } from './features/devices/devices.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    DevicesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
